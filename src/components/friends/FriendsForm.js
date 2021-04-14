a8c8c4a224b5be635905f8f9f5ecccb0a2225d6c
Chris Douglass <cdouglass516@gmail.com>
2021-04-14 08:16:21 -0500

    inital commit for the friends and articles components

---
+++ src/components/friends/FriendsForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { } from './modules/FriendsManager';
+import { useHistory } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { getUsers } from "./modules/FriendsManager";
+import { Hint } from "react-autocomplete-hint";
+
+export const FriendsForm = () =>{
+
+
+    return ``;
+}
