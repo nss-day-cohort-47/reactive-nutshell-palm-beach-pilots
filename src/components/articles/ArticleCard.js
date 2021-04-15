5708a8752eb0696ac78b1783adf3f6f26376f81d
Chris Douglass <cdouglass516@gmail.com>
2021-04-15 15:42:52 -0500

    Articles on the dom with links to article source.

--- src/components/articles/ArticleCard.js
+++ src/components/articles/ArticleCard.js
@@ -4,9 +4,15 @@ export const ArticleCard = ({ article, handleDel }) => {
   return (
       <div className="article_section">
         <section>
-            <div>{article.title}</div>
+            <div className="div_article__name">{article.title}</div>
             <div>{article.synopsis}</div>
+            <div className="articleButtons">
             <p><a href={article.url}  target="_blank" rel="noopener noreferrer">Article Link</a></p>
+            <a href="#" id="edit__"{...article.id} className="friendBtn btn_otherColor" onClick={() => handleDel(article.id)} >Edit Article</a>
+            <a href="#" id="delete__"{...article.id} className="friendBtn btn_otherColor" onClick={() => handleDel(article.id)} > Delete Article</a>
+
+
+            </div>
         </section>
       </div> 
  )
