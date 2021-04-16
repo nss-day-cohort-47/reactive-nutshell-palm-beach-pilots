b3b97eb4419fa61d50cf3a12c00f214f19486c21
Chris Douglass <cdouglass516@gmail.com>
2021-04-16 09:34:05 -0500

    Start to add to fuctionality

--- src/components/articles/ArticleCard.js
+++ src/components/articles/ArticleCard.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "../friends/friends.css";
 export const ArticleCard = ({ article, handleDel, handleEdit }) => {
-  return (
-      <div className="article_section">
-        <section>
-            <div className="div_article__name">{article.title}</div>
-            <div>{article.synopsis}</div>
-            <div className="articleButtons">
-            <p><a href={article.url}  target="_blank" rel="noopener noreferrer">Article Link</a></p>
-            <a href="#" id="edit__"{...article.id} className="friendBtn btn_otherColor" onClick={() => handleEdit(article.id)} >Edit Article</a>
-            <a href="#" id="delete__"{...article.id} className="friendBtn btn_otherColor" onClick={() => handleDel(article.id)} > Delete Article</a>
-
-
-            </div>
-        </section>
-      </div> 
- )
+    return (
+        <div className="article_section">
+            <section>
+                <div className="div_article__name">{article.title}</div>
+                <div>{article.synopsis}</div>
+                <div className="articleButtons">
+                    <p><a href={article.url} target="_blank" rel="noopener noreferrer">Article Link</a></p>
+                    <div className="friend_section">
+                        <a href="#" id="edit__"{...article.id} className="friendBtn btn_otherColor" onClick={() => handleEdit(article.id)} >Edit Article</a>
+                        <a href="#" id="delete__"{...article.id} className="friendBtn btn_Cancel" onClick={() => handleDel(article.id)} > Delete Article</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
